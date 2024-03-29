484eb9ee0bb63ca1022e1687cbfbec5e1b6382a8
parkeg1223 <72430547+parkeg1223@users.noreply.github.com>
2024-03-29 18:16:24 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 207ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240329/빙빙 돌며 숫자 사각형 채우기/snail-number-square.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n, m;
+    
+    private static boolean inRange(int x, int y) {
+        return (x >= 0 && x < n && y >= 0 && y < m);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        
+        int[][] field = new int[n][m];
+        field[0][0] = 1;
+        int x = 0, y = 0, dir = 1;
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, 1, 0, -1};
+        for (int i = 2; i <= n*m; i++) {
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
+            if (!inRange(nx, ny) || field[nx][ny] != 0) {
+                dir = (dir + 1) % 4;
+            }
+
+            x += dx[dir];
+            y += dy[dir];
+            field[x][y] = i;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(field[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
