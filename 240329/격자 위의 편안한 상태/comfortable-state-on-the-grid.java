9941cca22dd15aafee9aa13b3c5627dfe2a8e7fa
parkeg1223 <72430547+parkeg1223@users.noreply.github.com>
2024-03-29 18:59:08 +0900

    Title: 격자 위의 편안한 상태, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240329/격자 위의 편안한 상태/comfortable-state-on-the-grid.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int N, M;
+
+    private static boolean inRange(int x, int y) {
+        return (x >= 0 && x < N && y >= 0 && y < M);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, 1, 0, -1};
+        int[][] field = new int[N][N];
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(in.readLine());
+            int r = Integer.parseInt(st.nextToken()) - 1;
+            int c = Integer.parseInt(st.nextToken()) - 1;
+            field[r][c] = 1;
+            int cnt = 0;
+            for (int k = 0; k < 4; k++) {
+                int nx = r + dx[k];
+                int ny = c + dy[k];
+                if (inRange(nx, ny) && field[nx][ny] == 1) cnt++;
+            }
+            if (cnt == 3) System.out.println(1);
+            else System.out.println(0);
+        }
+    }
+}
