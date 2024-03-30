6f140fac5af1a916bd604b520b160d06078f5b24
parkeg1223 <72430547+parkeg1223@users.noreply.github.com>
2024-03-30 21:56:00 +0900

    Title: 총점 비교, Time: 106ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240330/총점 비교/compare-total-points.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+class Student implements Comparable<Student> {
+    String name;
+    int korean, english, math;
+
+    public Student() {}
+    public Student(String name, int korean, int english, int math) {
+        this.name = name;
+        this.korean = korean;
+        this.english = english;
+        this.math = math;
+    }
+
+    public void getInfo() {
+        System.out.println(this.name + " " + this.korean + " " + this.english + " " + this.math);
+    }
+
+    @Override
+    public int compareTo(Student s) {
+        return (this.korean + this.english + this.math) - (s.korean + s.english + s.math);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(in.readLine());
+
+        Student[] students = new Student[n];
+        StringTokenizer st;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(in.readLine());
+            students[i] = new Student(
+                st.nextToken(),
+                Integer.parseInt(st.nextToken()),
+                Integer.parseInt(st.nextToken()),
+                Integer.parseInt(st.nextToken())
+            );
+        }
+
+        Arrays.sort(students);
+        for (int i = 0; i < n; i++) {
+            students[i].getInfo();
+        }
+    }
+}
