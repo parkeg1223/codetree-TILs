530e8788971cdf5c298ba39444b23fab295049a2
parkeg1223 <72430547+parkeg1223@users.noreply.github.com>
2024-04-01 20:53:44 +0900

    Title: 행복한 수열의 개수, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240401/행복한 수열의 개수/number-of-happy-sequence.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static int n, m;
+    public static int[][] field;
+
+    public static int isHappySequence(int row, int col) {
+        int prevNum, consec = 0;
+        if (row == -1) {
+            prevNum = field[0][col];
+            for (int i = 0; i < n; i++) {
+                if (field[i][col] == prevNum) {
+                    if (++consec >= m) return 1;
+                } else {
+                    prevNum = field[i][col];
+                    consec = 1;
+                }
+            }
+        } else {
+            prevNum = field[row][0];
+            for (int j = 0; j < n; j++) {
+                if (field[row][j] == prevNum) {
+                        if (++consec >= m) return 1;
+                } else {
+                    prevNum = field[row][j];
+                    consec = 1;
+                }
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        field = new int[n][n];
+
+        int numberOfHappySequence = 0;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(in.readLine());
+            for (int j = 0; j < n; j++) {
+                field[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            numberOfHappySequence += (isHappySequence(-1, i) + isHappySequence(i, -1));
+        }
+
+        System.out.println(numberOfHappySequence);
+    }
+}
