be403a78e29589fe7d0ddf4574bcededf5d0a0d3
parkeg1223 <72430547+parkeg1223@users.noreply.github.com>
2024-04-02 03:13:38 +0900

    Title: 금 채굴하기, Time: 95ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240402/금 채굴하기/gold-mining.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n, m;
+    static int[][] field;
+
+    public static int getNumberOfGolds(int row, int col, int K) {
+        int numOfGolds = 0;
+        for (int i = row-K; i <= row+K; i++) {
+            int temp = K-Math.abs(i-row);
+            for (int j = col-temp; j <= col+temp; j++) {
+                if (i < 0 || i >= n || j < 0 || j >= n) continue;
+                numOfGolds += field[i][j];
+            }
+        }
+
+        return numOfGolds;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        field = new int[n][n];
+        int numOfGold = 0;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(in.readLine());
+            for (int j = 0; j < n; j++) {
+                field[i][j] = Integer.parseInt(st.nextToken());
+                if (field[i][j] == 1) numOfGold++;
+            }
+        }
+
+        int price = 0, maxK = 0;
+        while (maxK * maxK + (maxK+1) * (maxK+1) <= numOfGold * m) maxK++;
+
+        int maxNumOfGolds = 0;
+        for (int K = 0; K < maxK; K++) {
+            price = K*K + (K+1)*(K+1);
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    int value = getNumberOfGolds(i, j, K);
+                    if (maxNumOfGolds < value && price < value * m) maxNumOfGolds = value;
+                }
+            }
+        }
+
+        System.out.println(maxNumOfGolds);
+    }
+}
