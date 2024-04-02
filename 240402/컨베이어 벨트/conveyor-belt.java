d1ce1e091f9d04961ce88178da7d9d1534387bd1
parkeg1223 <72430547+parkeg1223@users.noreply.github.com>
2024-04-02 23:37:12 +0900

    Title: 컨베이어 벨트, Time: 98ms, Memory: 9MB, Status: Passed - CodeTree

--- 240402/컨베이어 벨트/conveyor-belt.java
+++ 240402/컨베이어 벨트/conveyor-belt.java
@@ -11,26 +11,22 @@ public class Main {
 
         n = Integer.parseInt(st.nextToken());
         t = Integer.parseInt(st.nextToken());
-        int[] numbers = new int[2*n];
-        int[] answer = new int[2*n];
+        ArrayDeque<Integer> numbers = new ArrayDeque<>();
         int nMove = t % (2*n);
         for (int i = 0; i < 2; i++) {
             st = new StringTokenizer(in.readLine());
             for (int j = 0; j < n; j++) {
-                numbers[i*n + j] = Integer.parseInt(st.nextToken());
+                numbers.offer(Integer.parseInt(st.nextToken()));
             }
         }
 
         for (int i = 0; i < nMove; i++) {
-            answer[i] = numbers[2*n - nMove + i];
-        }
-        for (int i = nMove; i < 2*n; i++) {
-            answer[i] = numbers[i-nMove];
+            numbers.offerFirst((numbers.pollLast()));
         }
 
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < n; j++) {
-                sb.append(answer[i*n + j]).append(' ');
+                sb.append(numbers.poll()).append(' ');
             }
             sb.append("\n");
         }
