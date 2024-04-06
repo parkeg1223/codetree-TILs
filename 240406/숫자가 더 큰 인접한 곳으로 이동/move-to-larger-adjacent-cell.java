a91a27972c3883d26dbcfb198f421dbe72b9f604
parkeg1223 <72430547+parkeg1223@users.noreply.github.com>
2024-04-06 22:58:18 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: 106ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240406/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int n, r, c, currX, currY;
+	static int[] dx = {-1, 1, 0, 0};
+	static int[] dy = {0, 0, -1, 1};
+	static int[][] field;
+	
+	public static boolean inRange(int x, int y) {
+		return (x >= 0 && x < n && y >= 0 && y < n);
+	}
+	
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        n = Integer.parseInt(st.nextToken());
+        field = new int[n][n];
+        currX = r = Integer.parseInt(st.nextToken()) - 1;
+        currY = c = Integer.parseInt(st.nextToken()) - 1;
+
+        for (int i = 0; i < n; i++) {
+        	st = new StringTokenizer(in.readLine());
+        	for (int j = 0; j < n; j++) {
+        		field[i][j] = Integer.parseInt(st.nextToken());
+        	}
+        }
+        
+        while (true) {
+        	sb.append(field[currX][currY]).append(' ');
+        	boolean isProceedable = false;
+        	for (int i = 0; i < 4; i++) {
+        		int nx = currX + dx[i];
+        		int ny = currY + dy[i];
+        		if (inRange(nx, ny) && field[nx][ny] > field[currX][currY]) {
+        			currX = nx; currY = ny;
+        			isProceedable = true;
+        			break;
+        		}
+        	}
+        	if (!isProceedable) break;
+        }
+        
+        System.out.println(sb.toString());
+    }
+
+}
