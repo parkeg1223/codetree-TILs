89d2049bec1f6480b96576154e3bfc2792bcc708
parkeg1223 <72430547+parkeg1223@users.noreply.github.com>
2024-04-07 01:38:06 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 108ms, Memory: 9MB, Status: Passed - CodeTree

--- 240407/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.java
+++ 240407/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.java
@@ -6,7 +6,7 @@ public class Main {
 	static int[] dx = {0, -1, 0, 1};
 	static int[] dy = {1, 0, -1, 0};
 	static char[][] field;
-	static boolean[][] visited;
+	static boolean[][][] visited;
 	
 	public static boolean inRange(int x, int y) {
 		return (x >= 0 && x < N && y >= 0 && y < N);
@@ -16,7 +16,7 @@ public class Main {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(in.readLine().trim());
         field = new char[N][N];
-        visited = new boolean[N][N];
+        visited = new boolean[N][N][4];
         
         StringTokenizer st = new StringTokenizer(in.readLine());
         x = Integer.parseInt(st.nextToken()) - 1;
@@ -26,28 +26,28 @@ public class Main {
     		field[i] = in.readLine().toCharArray();
         }
 
-        visited[x][y] = true;
+        visited[x][y][dir] = true;
         int currX = x, currY = y, time = 0;
         while (true) {
         	time++;
-        	int nx = 0, ny = 0;
-        	for (int i = 0; i < 4; i++) {
+        	int nx = 0, ny = 0, loop = 0;
+        	for (loop = 0; loop < 4; loop++) {
         		nx = currX + dx[dir];
             	ny = currY + dy[dir];
             	if (!inRange(nx, ny)) {
             		System.out.println(time);
             		return;
-            	} else if (field[nx][ny] == '#') dir++;
+            	} else if (field[nx][ny] == '#') dir = (dir + 1) % 4;
             	else break;
         	}
-        	if (dir == 4) {
+        	if (loop == 4) {
         		System.out.println(-1);
         		return;
         	} else {
-        		if (visited[nx][ny]) {
+        		if (visited[nx][ny][dir]) {
         			System.out.println(-1);
         			return;
-        		} else visited[nx][ny] = true;
+        		} else visited[nx][ny][dir] = true;
         		if (field[nx + dx[(dir+3)%4]][ny + dy[(dir+3)%4]] == '#') {
         			currX = nx; currY = ny;
             	} else {
@@ -55,10 +55,10 @@ public class Main {
             		dir = (dir + 3) % 4;
             		currX = nx + dx[dir];
             		currY = ny + dy[dir];
-            		if (visited[currX][currY]) {
+            		if (visited[currX][currY][dir]) {
             			System.out.println(-1);
             			return;
-            		} else visited[currX][currY] = true;
+            		} else visited[currX][currY][dir] = true;
             	}
         	}
         }
