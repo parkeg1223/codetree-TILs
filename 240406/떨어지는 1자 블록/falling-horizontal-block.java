4cbece6b2b4c971ee21c1a516530d1f6beaeb525
parkeg1223 <72430547+parkeg1223@users.noreply.github.com>
2024-04-06 23:13:18 +0900

    Title: 떨어지는 1자 블록, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240406/떨어지는 1자 블록/falling-horizontal-block.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken()) - 1;
+        int[][] field = new int[n][n];
+        int[] highestIdxs = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+        	st = new StringTokenizer(in.readLine());
+        	for (int j = 0; j < n; j++) {
+        		field[i][j] = Integer.parseInt(st.nextToken());
+        		if (highestIdxs[j] == 0 && field[i][j] == 1) {
+        			highestIdxs[j] = i;
+        		}
+        	}
+        }
+        
+        int highstIdx = n;
+        for (int i = k; i < k+m; i++) {
+        	highstIdx = Math.min(highstIdx, highestIdxs[i]);
+        }
+        
+        Arrays.fill(field[highstIdx-1], k, k+m, 1);
+        
+        for (int i = 0; i < n; i++) {
+        	for (int j = 0; j < n; j++) {
+        		sb.append(field[i][j]).append(' ');
+        	}
+        	sb.append('\n');
+        }
+        System.out.println(sb.toString());
+        
+    }
+}
