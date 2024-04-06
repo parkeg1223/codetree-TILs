c14713df9df36ab60cce705a8a9cb6eeddbabd9a
parkeg1223 <72430547+parkeg1223@users.noreply.github.com>
2024-04-07 01:48:46 +0900

    fix: if-else 로직 수정

--- 240407/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.java
+++ 240407/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.java
@@ -33,34 +33,36 @@ public class Main {
         	int nx = 0, ny = 0, loop = 0;
         	for (loop = 0; loop < 4; loop++) {
         		nx = currX + dx[dir];
-            	ny = currY + dy[dir];
-            	if (!inRange(nx, ny)) {
-            		System.out.println(time);
-            		return;
-            	} else if (field[nx][ny] == '#') dir = (dir + 1) % 4;
-            	else break;
+            		ny = currY + dy[dir];
+            		if (!inRange(nx, ny)) {
+            			System.out.println(time);
+            			return;
+            		} else if (field[nx][ny] == '#') dir = (dir + 1) % 4;
+            		else break;
         	}
+		
         	if (loop == 4) {
         		System.out.println(-1);
         		return;
-        	} else {
-        		if (visited[nx][ny][dir]) {
-        			System.out.println(-1);
-        			return;
-        		} else visited[nx][ny][dir] = true;
-        		if (field[nx + dx[(dir+3)%4]][ny + dy[(dir+3)%4]] == '#') {
-        			currX = nx; currY = ny;
-	            	} else {
-	            		time++;
-	            		dir = (dir + 3) % 4;
-	            		currX = nx + dx[dir];
-	            		currY = ny + dy[dir];
-	            		if (visited[currX][currY][dir]) {
-	            			System.out.println(-1);
-	            			return;
-	            		} else visited[currX][currY][dir] = true;
-	            	}
         	}
+		
+		if (visited[nx][ny][dir]) {
+			System.out.println(-1);
+			return;
+		} else visited[nx][ny][dir] = true;
+		
+		if (field[nx + dx[(dir+3)%4]][ny + dy[(dir+3)%4]] == '#') {
+			currX = nx; currY = ny;
+		} else {
+			time++;
+			dir = (dir + 3) % 4;
+			currX = nx + dx[dir];
+			currY = ny + dy[dir];
+			if (visited[currX][currY][dir]) {
+				System.out.println(-1);
+				return;
+			} else visited[currX][currY][dir] = true;
+		}
         }
     }
 }
